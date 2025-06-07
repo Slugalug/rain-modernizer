1cbd42ed0805d3f7e64a31753a6ebab1049339bd
Slugalug <amandamartin.art@gmail.com>
2025-06-07 14:34:48 -0500

    Update master.js

--- master.js
+++ master.js
@@ -1,29 +1,32 @@
-const waitForElement = (selector, callback) => {
-  const interval = setInterval(() => {
-    const el = document.querySelector(selector);
-    if (el) {
-      clearInterval(interval);
-      callback(el);
+(function waitForAngular() {
+  const check = setInterval(() => {
+    if (window.angular && document.querySelector('[ng-app]')) {
+      clearInterval(check);
+      const appElement = document.querySelector('[ng-app]');
+      const $injector = angular.element(appElement).injector();
+      const $rootScope = $injector.get('$rootScope');
+      
+      $rootScope.$on('$viewContentLoaded', function () {
+        console.log('Angular fully loaded! Injecting header.');
+        
+        if (!document.querySelector('#CustomHeader')) {
+          const headerHTML = `
+            <header id="CustomHeader">
+              <div class="wrapper">
+                <div style="font-family: Oswald, sans-serif; font-size: 28px; font-weight: bold; color: #C70039;">
+                  Boxcar Quilts
+                </div>
+                <nav>
+                  <a href="/shop">Shop</a>
+                  <a href="/classes">Classes</a>
+                  <a href="/contact">Contact</a>
+                </nav>
+              </div>
+            </header>
+          `;
+          document.body.insertAdjacentHTML('afterbegin', headerHTML);
+        }
+      });
     }
-  }, 200);
-};
-
-waitForElement('#PageContent', function() {
-  if (!document.querySelector('#CustomHeader')) {
-    const headerHTML = `
-      <header id="CustomHeader">
-        <div class="wrapper">
-          <div style="font-family: Oswald, sans-serif; font-size: 28px; font-weight: bold; color: #C70039;">
-            Boxcar Quilts
-          </div>
-          <nav>
-            <a href="/shop">Shop</a>
-            <a href="/classes">Classes</a>
-            <a href="/contact">Contact</a>
-          </nav>
-        </div>
-      </header>
-    `;
-    document.body.insertAdjacentHTML('afterbegin', headerHTML);
-  }
-});
+  }, 100);
+})();
